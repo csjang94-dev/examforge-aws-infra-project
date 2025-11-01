1039ca0dade66c46179edd5e233c7466d65aa0c3
csjang94-dev <csjang94@gmail.com>
2025-11-01 16:28:08 +0900

    Delete modules directory

--- modules/vpc/variables.tf
+++
@@ -1,7 +0,0 @@
-
-variable "environment" { type = string }
-variable "vpc_cidr" { type = string}
-variable "availability_zones" { type = list(string)}
-variable "public_subnet_cidrs" { type = list(string)}
-variable "private_subnet_cidrs" { type= list(string)}
-variable "create_nat_gateway" { type = bool }
