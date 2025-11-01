36c76e730fab9c244d234083644b2442aa9fba91
csjang94-dev <csjang94@gmail.com>
2025-11-02 07:56:05 +0900

    Update run_apache.sh

--- examforge/run_apache.sh
+++ examforge/run_apache.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
+# run_apache.sh
 # 컨테이너에서 Apache HTTP 서버 실행 스크립트
 
+# ---------------------------
+# Apache 환경 변수
+# ---------------------------
 APACHE_RUN_USER=www-data
 APACHE_RUN_GROUP=www-data
 APACHE_LOG_DIR=/var/log/apache2
 
+# ---------------------------
 # 로그/락 디렉토리 생성
-mkdir -p $APACHE_LOG_DIR /var/run/httpd /var/lock/httpd
+# ---------------------------
+mkdir -p "$APACHE_LOG_DIR" /var/run/httpd /var/lock/httpd
 
+# ---------------------------
 # Apache 포트 3000으로 변경
+# ---------------------------
 sed -i 's/Listen 80/Listen 3000/' /etc/httpd/conf/httpd.conf
 sed -i 's/<VirtualHost \*:80>/<VirtualHost *:3000>/' /etc/httpd/conf.d/*.conf
 
+# ---------------------------
 # DocumentRoot를 examforge 폴더로 변경
+# ---------------------------
 sed -i 's#DocumentRoot "/var/www/html"#DocumentRoot "/examforge"#' /etc/httpd/conf/httpd.conf
 sed -i 's#<Directory "/var/www/html">#<Directory "/examforge">#' /etc/httpd/conf/httpd.conf
 
+# ---------------------------
 # Apache 시작 (포그라운드 실행)
-/usr/sbin/httpd -D FOREGROUND
+# ---------------------------
+exec /usr/sbin/httpd -D FOREGROUND
